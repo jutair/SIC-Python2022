993d44400cf80203ca51d94526707623167c5519
Jutair Rios <84020383+jutair@users.noreply.github.com>
2022-08-03 19:41:07 -0300

    Update 4_30_de_julho.py

--- 30_julho/4_30_de_julho.py
+++ 30_julho/4_30_de_julho.py
@@ -1,14 +1,17 @@
-#lista_primos =[]
-#lista_compostos= []
-for i in range (2, 13):
+def listaprimos(inicio,fim):
+  lista_primos =[]
+  lista_compostos= []
+  for i in range (inicio, fim):
       n = i
       total= 0
       for i in range(1, n+1): 
         if n %i ==0:
           total += 1
       if total ==2:
-          #lista_primos.append(n)
-          print(n, "É primo!")
+          lista_primos.append(n)
+          #print(n, "É primo!")
       else:
-          print(n, "É composto!")
-          #lista_compostos.append(n)
+          #print(n, "É composto!")
+          lista_compostos.append(n)
+  print("A lista de números primos no intervado de:", inicio, "a", fim, "=", lista_primos)
+  print("A lista de números primos no compostos de:", inicio, "a", fim, "=", lista_compostos)
